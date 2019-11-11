266596872b795a270bd706788da1935309d40bf7
Evgen <1303evgen1998@gmail.com>
2019-11-11 20:27:09 +0200

    test

--- src/main/java/com/hunghost/onpusocial/SecurityConfig.java
+++ src/main/java/com/hunghost/onpusocial/SecurityConfig.java
@@ -6,19 +6,13 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-import java.security.AuthProvider;
-import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -36,12 +30,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             .anyRequest().authenticated()
             .and()
             .httpBasic()
+                .and()
+                .logout().logoutSuccessUrl("/users")
 //            .and()
 //            .sessionManagement()
 //            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
             .and()
             .csrf().disable()
             .formLogin().disable();
+
     }
 
     @Bean
