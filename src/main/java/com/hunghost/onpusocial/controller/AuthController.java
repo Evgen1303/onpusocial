227c3e11b76a7324ec19da7230bcf7c85701a47c
Evgen <1303evgen1998@gmail.com>
2019-11-18 14:29:35 +0200

    Refactor code

--- src/main/java/com/hunghost/onpusocial/controller/AuthController.java
+++ src/main/java/com/hunghost/onpusocial/controller/AuthController.java
@@ -1,33 +1,15 @@
 package com.hunghost.onpusocial.controller;
 
-
         import com.hunghost.onpusocial.security.CustomAuthenticationManager;
         import com.hunghost.onpusocial.entity.User;
-        import com.hunghost.onpusocial.security.UserAuthService;
         import com.hunghost.onpusocial.service.user.UserCommandService;
         import com.hunghost.onpusocial.service.user.UserQueryService;
-        import org.apache.logging.log4j.LogManager;
-        import org.apache.logging.log4j.Logger;
         import org.springframework.beans.factory.annotation.Autowired;
         import org.springframework.http.HttpStatus;
-        import org.springframework.http.ResponseEntity;
-        import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-        import org.springframework.security.core.Authentication;
-        import org.springframework.security.core.context.SecurityContext;
-        import org.springframework.security.core.context.SecurityContextHolder;
         import org.springframework.security.core.session.SessionRegistry;
-        import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
         import org.springframework.web.bind.annotation.*;
-
-        import javax.servlet.http.HttpServletRequest;
-        import javax.servlet.http.HttpServletResponse;
-        import javax.servlet.http.HttpSession;
-
-
         import java.util.Collection;
         import java.util.List;
-
-        import static org.springframework.security.web.context.HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY;
 
 @RestController
 @CrossOrigin
@@ -38,11 +20,6 @@ public class AuthController {
     private UserCommandService userCommandService;
     @Autowired
     private SessionRegistry sessionRegistry;
-
-    private static final Logger log = LogManager.getLogger(UserAuthService.class);
-    @Autowired
-    HttpServletRequest req;
-    HttpServletResponse response;
     @Autowired
     private CustomAuthenticationManager customAuthenticationManager;
 
@@ -55,17 +32,8 @@ public class AuthController {
     @GetMapping("login")
     @ResponseBody
     public Boolean login(@RequestParam String login, @RequestParam String password) {
-        UsernamePasswordAuthenticationToken authReq
-                = new UsernamePasswordAuthenticationToken(login,password);
-        Authentication auth = customAuthenticationManager.authenticate(authReq);
-        SecurityContext sc = SecurityContextHolder.getContext();
-        sc.setAuthentication(auth);
 
-        HttpSession session = req.getSession(true);
-        sessionRegistry.registerNewSession(session.getId(), SecurityContextHolder.getContext().getAuthentication().getPrincipal());
-        log.info("Login session: "+ session.getId());
-        session.setAttribute(SPRING_SECURITY_CONTEXT_KEY, sc);
-        return sc.getAuthentication().isAuthenticated();
+        return customAuthenticationManager.login(login,password);
     }
 
     @GetMapping("authuser")
--- src/main/java/com/hunghost/onpusocial/security/CustomAuthenticationManager.java
+++ src/main/java/com/hunghost/onpusocial/security/CustomAuthenticationManager.java
@@ -12,30 +12,39 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.session.SessionRegistry;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.util.Collection;
 import java.util.stream.Collectors;
+
+import static org.springframework.security.web.context.HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY;
+
 @Service
 public class CustomAuthenticationManager implements AuthenticationManager {
-
+    @Autowired
+    private SessionRegistry sessionRegistry;
     private UserRepository userRepository;
     private static final Logger log = LogManager.getLogger(UserAuthService.class);
-
+    @Autowired
+    HttpServletRequest req;
+    HttpServletResponse response;
 
     @Autowired
     public CustomAuthenticationManager(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
-
-
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
         String username = authentication.getPrincipal() + "";
         String password = authentication.getCredentials() + "";
-
         User user = userRepository.findByUsername(username);
         if (user == null) {
             throw new UsernameNotFoundException("Invalid username or password.");
@@ -48,12 +57,23 @@ public class CustomAuthenticationManager implements AuthenticationManager {
             return new UsernamePasswordAuthenticationToken(username, password, mapRolesToAuthorities(user.getAuthorities()));
         }
     }
-
     private Collection<? extends GrantedAuthority> mapRolesToAuthorities(Collection<Role> roles){
         return roles.stream()
                 .map(role -> new SimpleGrantedAuthority(role.getName()))
                 .collect(Collectors.toList());
 
+    }
+        public Boolean login (String login, String password){
+        UsernamePasswordAuthenticationToken authReq
+                = new UsernamePasswordAuthenticationToken(login,password);
+        Authentication auth = this.authenticate(authReq);
+        SecurityContext sc = SecurityContextHolder.getContext();
+        sc.setAuthentication(auth);
+        HttpSession session = req.getSession(true);
+        sessionRegistry.registerNewSession(session.getId(), SecurityContextHolder.getContext().getAuthentication().getPrincipal());
+        log.info("Login session: "+ session.getId());
+        session.setAttribute(SPRING_SECURITY_CONTEXT_KEY, sc);
+        return sc.getAuthentication().isAuthenticated();
     }
 
 }
