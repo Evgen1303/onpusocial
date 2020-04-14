73b0c6b870b224e034399bca06291a33a87240be
Evgen <1303evgen1998@gmail.com>
2020-04-14 11:13:01 +0300

    fix create kafedra and group

--- src/main/java/com/hunghost/onpusocial/service/kafedra/KafedraConverterService.java
+++ src/main/java/com/hunghost/onpusocial/service/kafedra/KafedraConverterService.java
@@ -2,12 +2,22 @@ package com.hunghost.onpusocial.service.kafedra;
 
 import com.hunghost.onpusocial.dto.KafedraDTO;
 import com.hunghost.onpusocial.entity.Kafedra;
+import com.hunghost.onpusocial.service.faculty.FacultyQueryService;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class KafedraConverterService {
     private ModelMapper modelMapper = new ModelMapper();
+    private FacultyQueryService facultyQueryService;
+
+    @Autowired
+    public KafedraConverterService(FacultyQueryService facultyQueryService) {
+        this.facultyQueryService = facultyQueryService;
+    }
+
+
 
     public KafedraDTO convertToDto(Kafedra kafedra) {
         return modelMapper.map(kafedra, KafedraDTO.class);
@@ -15,6 +25,7 @@ public class KafedraConverterService {
 
     public  Kafedra convertToEntity(KafedraDTO kafedraDTO) {
         Kafedra kafedra = modelMapper.map(kafedraDTO, Kafedra.class);
+        kafedra.setFaculty(facultyQueryService.getFacultyById(kafedraDTO.getFaculty()));
         return kafedra;
 
     }
--- src/main/java/com/hunghost/onpusocial/service/studygroup/StudyGroupConverterService.java
+++ src/main/java/com/hunghost/onpusocial/service/studygroup/StudyGroupConverterService.java
@@ -2,12 +2,21 @@ package com.hunghost.onpusocial.service.studygroup;
 
 import com.hunghost.onpusocial.dto.StudygroupDTO;
 import com.hunghost.onpusocial.entity.Studygroup;
+import com.hunghost.onpusocial.service.kafedra.KafedraQueryService;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class StudyGroupConverterService {
     private ModelMapper modelMapper = new ModelMapper();
+
+    private KafedraQueryService kafedraQueryService;
+
+    @Autowired
+    public StudyGroupConverterService(KafedraQueryService kafedraQueryService) {
+        this.kafedraQueryService = kafedraQueryService;
+    }
 
     public StudygroupDTO convertToDto(Studygroup studygroup) {
         return modelMapper.map(studygroup, StudygroupDTO.class);
@@ -15,6 +24,7 @@ public class StudyGroupConverterService {
 
     public  Studygroup convertToEntity(StudygroupDTO studygroupDTO) {
         Studygroup studygroup = modelMapper.map(studygroupDTO, Studygroup.class);
+        studygroup.setKafedra(kafedraQueryService.getKafedraById(studygroupDTO.getKafedra()));
         return studygroup;
 
     }
